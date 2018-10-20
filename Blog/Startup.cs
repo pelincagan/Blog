35dcf6bb41fbc588b92df89be53999edbd4708ed
Pelin Cagan <pelincagan@msn.com>
2018-10-20 20:10:36 +0300

    Initial commit

---
+++ Blog/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Blog.Startup))]
+
+namespace Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
